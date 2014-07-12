c5841eea4a55033f7e9ff262214e433a0b262b6f
Polamin Singhasuwich <polaminggkub@gmail.com>
2014-07-12 22:46:50 +0800

    First complete Class struct with vector

---
+++ C++ struct vector iterator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class subject {
+	
+	public:
+    string name;
+    int marks;
+    int credits;
+};
+
+
+
+int main() {
+    vector<subject> sub;
+
+    //Push back new subject created with default constructor.
+    sub.push_back(subject());
+
+    //Vector now has 1 element @ index 0, so modify it.
+    sub[0].name = "english";
+    sub[0].marks = 80;
+
+    //Add a new element if you want another:
+    sub.push_back(subject());
+
+    //Modify its name and marks.
+    sub[1].name = "math";
+    sub[1].marks = 90;
+
+    
+
+    for (int i = 0 ; i < sub.size() ; i++)
+    {
+    	cout << sub[i].name << endl;
+    }
+
+}
